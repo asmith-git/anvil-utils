471aec047509b2e86896ff56e5acbe102770916c
Adam Smith <adam.george.smith@btinternet.com>
2019-09-24 21:09:00 +0100

    Comments and a memcpy bugfix

--- include/anvil/lutils/PODAlgorithm.hpp
+++ include/anvil/lutils/PODAlgorithm.hpp
@@ -32,27 +32,35 @@ namespace anvil { namespace lutils {
 	static void transform(const T* input_begin, const T* const input_end, T* output_begin, const F& unary_op) {
 		const uint32_t count = static_cast<uint32_t>(input_end - input_begin);
 		if (input_begin == output_begin) {
+			// Call the in-place transformation algorithm
 			transform<F, T>(output_begin, output_begin + count, unary_op);
 		} else {
-			enum {
-				CACHE_BLOCK = 4096, // 4 KiB
-				CACHE_BLOCK_COUNT = CACHE_BLOCK / sizeof(T)
-			};
-			if constexpr (std::is_pod<T>::value && sizeof(T) < CACHE_BLOCK) {
-				const uint32_t cacheAligned = count / CACHE_BLOCK_COUNT;
-				const uint32_t cacheUnaligned = count % CACHE_BLOCK_COUNT;
+			enum { TARGET_CACHE_BLOCK = 4096 };
+			// If working on a small POD type
+			if constexpr (std::is_pod<T>::value && sizeof(T) < TARGET_CACHE_BLOCK) {
+				// Divide the work into blocks of 4 KiB
+				enum {
+					CACHE_BLOCK_COUNT = TARGET_CACHE_BLOCK / sizeof(T),
+					ACTUAL_CACHE_BLOCK CACHE_BLOCK_COUNT * sizeof(T)
+				};
 
+				// Process the alligned blocks
+				const uint32_t cacheAligned = count / CACHE_BLOCK_COUNT;
 				for (uint32_t i = 0u; i < cacheAligned; ++i) {
-					std::memcpy(output_begin, input_begin, CACHE_BLOCK);
+					std::memcpy(output_begin, input_begin, ACTUAL_CACHE_BLOCK);
 					T* const output_end = output_begin + CACHE_BLOCK_COUNT;
 					transform<F, T>(output_begin, output_end, unary_op);
 					output_begin = output_end;
 				}
+
+				// Process a remaining unaligned block if one exists
+				const uint32_t cacheUnaligned = count % CACHE_BLOCK_COUNT;
 				if (cacheUnaligned != 0u) {
 					std::memcpy(output_begin, input_begin, cacheUnaligned * sizeof(T));
 					transform<F, T>(output_begin, output_begin + cacheUnaligned, unary_op);
 				}
 			} else {
+				// Process one item at a time with a copy
 				for (uint32_t i = 0u; i < count; ++i) {
 					output_begin[i] = input_begin[i];
 					unary_op(output_begin[i]);
