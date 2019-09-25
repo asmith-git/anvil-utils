80c43f65d7c8c673ff171ff5c21e44607aeb189f
Adam Smith <adam.george.smith@btinternet.com>
2019-09-25 19:35:11 +0100

    Optimisations

--- include/anvil/lutils/PODAlgorithm.hpp
+++ include/anvil/lutils/PODAlgorithm.hpp
@@ -19,6 +19,49 @@
 #include "anvil/lutils/Alignment.hpp"
 
 namespace anvil { namespace lutils {
+
+	//template<size_t BYTES>
+	//static inline void FastMemcpy_avx512(void* const dst, const void* src) throw() {
+	//	static_assert((BYTES % 64u) == 0, "Memory block must be a multiple of 64u bytes");
+	//	__m512 xmm0;
+	//	for (size_t i = 0u; i < BYTES; i += 64u) {
+	//		xmm0 = _mm512_loadu_ps(reinterpret_cast<const float*>(static_cast<const uint8_t*>(src) + i));
+	//		_mm512_storeu_ps(reinterpret_cast<float*>(static_cast<uint8_t*>(dst) + i), xmm0);
+	//	}
+	//}
+
+	//template<size_t BYTES>
+	//static inline void FastMemcpy_avx(void* const dst, const void* src) throw() {
+	//	static_assert((BYTES % 32u) == 0, "Memory block must be a multiple of 32 bytes");
+	//	__m256 xmm0;
+	//	for (size_t i = 0u; i < BYTES; i += 32u) {
+	//		xmm0 = _mm256_loadu_ps(reinterpret_cast<const float*>(static_cast<const uint8_t*>(src) + i));
+	//		_mm256_storeu_ps(reinterpret_cast<float*>(static_cast<uint8_t*>(dst) + i), xmm0);
+	//	}
+	//}
+
+	//template<size_t BYTES>
+	//static inline void FastMemcpy_sse(void* const dst, const void* src) throw() {
+	//	static_assert((BYTES % 16u) == 0, "Memory block must be a multiple of 16 bytes");
+	//	__m128 xmm0;
+	//	for (size_t i = 0u; i < BYTES; i += 16u) {
+	//		xmm0 = _mm_loadu_ps(reinterpret_cast<const float*>(static_cast<const uint8_t*>(src) + i));
+	//		_mm_storeu_ps(reinterpret_cast<float*>(static_cast<uint8_t*>(dst) + i), xmm0);
+	//	}
+	//}
+
+	template<size_t BYTES>
+	static inline void FastMemcpy(void* const dst, const void* src) throw() {
+		//if constexpr ((BYTES % 64u) == 0) {
+		//	FastMemcpy_avx512(dst, src);
+		//} else if constexpr ((BYTES % 32u) == 0) {
+		//	FastMemcpy_avx(dst, src);
+		//} else if constexpr ((BYTES % 16u) == 0) {
+		//	FastMemcpy_sse(dst, src);
+		//} else {
+			std::memcpy(dst, src, BYTES);
+		//}
+	}
 	
 	template<class F, class T>
 	static void transform(T* begin, const T* const end, const F& unary_op) {
@@ -35,7 +78,7 @@ namespace anvil { namespace lutils {
 			// Call the in-place transformation algorithm
 			transform<F, T>(output_begin, output_begin + count, unary_op);
 		} else {
-			enum : uint32_t { TARGET_CACHE_BLOCK = PAGE_SIZE_BYTES };
+			enum : uint32_t { TARGET_CACHE_BLOCK = CACHE_LINE_BYTES };
 			// If working on a small POD type
 			if constexpr (std::is_trivially_copyable<T>::value && sizeof(T) < TARGET_CACHE_BLOCK) {
 				// Divide the work into blocks of 4 KiB
@@ -47,7 +90,7 @@ namespace anvil { namespace lutils {
 				// Process the alligned blocks
 				const uint32_t cacheAligned = count / CACHE_BLOCK_COUNT;
 				for (uint32_t i = 0u; i < cacheAligned; ++i) {
-					std::memcpy(output_begin, input_begin, ACTUAL_CACHE_BLOCK);
+					FastMemcpy<ACTUAL_CACHE_BLOCK>(output_begin, input_begin);
 					T* const output_end = output_begin + CACHE_BLOCK_COUNT;
 					transform<F, T>(output_begin, output_end, unary_op);
 					output_begin = output_end;
