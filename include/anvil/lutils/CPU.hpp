38af436702de8a2459e4942b11f9cd5acfc7e93f
Adam Smith <adam.george.smith@btinternet.com>
2019-09-26 19:33:50 +0100

    x86 and x86_64 detection

--- include/anvil/lutils/CPU.hpp
+++ include/anvil/lutils/CPU.hpp
@@ -18,14 +18,24 @@
 #include <cstdint>
 
 #define ANVIL_CPU_UNKNOWN 0u
+#define ANVIL_CPU_X86 1u
+#define ANVIL_CPU_X86_64 2u
 
 #ifndef ANVIL_CPU_ARCHITECUTE
-#define ANVIL_CPU_ARCHITECUTE ANVIL_CPU_UNKNOWN
+	#if defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) || defined(_M_X64) || defined(_M_AMD64)
+		#define ANVIL_CPU_ARCHITECUTE ANVIL_CPU_X86_64
+	#elif defined(i386) ||  defined(__i386) ||  defined(__i386__) ||  defined(__i486__) ||  defined(__i586__) ||  defined(__i686__) ||  defined(__IA32__) ||  defined(_M_I86) ||  defined(_M_IX86) ||  defined(__X86__) ||  defined(_X86_) ||  defined(__THW_INTEL__) ||   defined(__INTEL__) ||   defined(__THW_INTEL__) ||   defined(__386)
+		#define ANVIL_CPU_ARCHITECUTE ANVIL_CPU_X86
+	#else
+		#define ANVIL_CPU_ARCHITECUTE ANVIL_CPU_UNKNOWN
+	#endif
 #endif
 
 namespace anvil {
 	enum CpuArchitecture : uint8_t {
 		CPU_UNKNOWN = ANVIL_CPU_UNKNOWN,
+		CPU_X86 = ANVIL_CPU_X86,
+		CPU_X86_64 = ANVIL_CPU_X86_64
 	};
 
 	static const constexpr CpuArchitecture CPU_ARCHITECUTE = static_cast<CpuArchitecture>(ANVIL_CPU_ARCHITECUTE);
