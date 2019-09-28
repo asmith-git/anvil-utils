f7d68dfa65749e3c68bfde1f26cc6a3c7f35e831
Adam Smith <adam.george.smith@btinternet.com>
2019-09-28 15:10:52 +0100

    Odd/Even round

--- include/anvil/lutils/Arithmetic.hpp
+++ include/anvil/lutils/Arithmetic.hpp
@@ -24,6 +24,18 @@ namespace anvil {
 	template<class T>
 	static bool IsEven(const T) throw();
 
+	template<class T>
+	static T RoundUpOdd(const T) throw();
+
+	template<class T>
+	static T RoundDownOdd(const T) throw();
+
+	template<class T>
+	static T RoundUpEven(const T) throw();
+
+	template<class T>
+	static T RoundDownEven(const T) throw();
+
 	// IsOdd
 
 	template<>
@@ -126,6 +138,222 @@ namespace anvil {
 	template<>
 	static inline bool IsEven<double>(const double value) throw() {
 		return IsEven<int64_t>(static_cast<int64_t>(value));
+	}
+
+	// RoundUpOdd
+
+	template<>
+	static inline uint64_t RoundUpOdd<uint64_t>(const uint64_t value) throw() {
+		return value | 1ull;
+	}
+
+	template<>
+	static inline uint32_t RoundUpOdd<uint32_t>(const uint32_t value) throw() {
+		return value | 1u;
+	}
+
+	template<>
+	static inline uint16_t RoundUpOdd<uint16_t>(const uint16_t value) throw() {
+		return value | 1u;
+	}
+
+	template<>
+	static inline uint8_t RoundUpOdd<uint8_t>(const uint8_t value) throw() {
+		return value | 1u;
+	}
+
+	template<>
+	static inline int64_t RoundUpOdd<int64_t>(const int64_t value) throw() {
+		return value | 1ll;
+	}
+
+	template<>
+	static inline int32_t RoundUpOdd<int32_t>(const int32_t value) throw() {
+		return value | 1;
+	}
+
+	template<>
+	static inline int16_t RoundUpOdd<int16_t>(const int16_t value) throw() {
+		return value | 1;
+	}
+
+	template<>
+	static inline int8_t RoundUpOdd<int8_t>(const int8_t value) throw() {
+		return value | 1;
+	}
+
+	template<>
+	static inline float RoundUpOdd<float>(const float value) throw() {
+		return static_cast<float>(RoundUpOdd(static_cast<int64_t>(value)));
+	}
+
+	template<>
+	static inline double RoundUpOdd<double>(const double value) throw() {
+		return static_cast<double>(RoundUpOdd(static_cast<int64_t>(value)));
+	}
+
+	// RoundDownOdd
+
+	template<>
+	static inline uint64_t RoundDownOdd<uint64_t>(const uint64_t value) throw() {
+		enum : uint64_t { MASK = ~1ull };
+		return value & MASK;
+	}
+
+	template<>
+	static inline uint32_t RoundDownOdd<uint32_t>(const uint32_t value) throw() {
+		enum : uint32_t { MASK = ~1u };
+		return value & MASK;
+	}
+
+	template<>
+	static inline uint16_t RoundDownOdd<uint16_t>(const uint16_t value) throw() {
+		enum : uint16_t { MASK = ~1u };
+		return value & MASK;
+	}
+
+	template<>
+	static inline uint8_t RoundDownOdd<uint8_t>(const uint8_t value) throw() {
+		enum : uint8_t { MASK = ~1u };
+		return value & MASK;
+	}
+
+	template<>
+	static inline int64_t RoundDownOdd<int64_t>(const int64_t value) throw() {
+		enum : int64_t { MASK = ~1ll };
+		return value & MASK;
+	}
+
+	template<>
+	static inline int32_t RoundDownOdd<int32_t>(const int32_t value) throw() {
+		enum : int32_t { MASK = ~1 };
+		return value & MASK;
+	}
+
+	template<>
+	static inline int16_t RoundDownOdd<int16_t>(const int16_t value) throw() {
+		enum : int16_t { MASK = ~1 };
+		return value & MASK;
+	}
+
+	template<>
+	static inline int8_t RoundDownOdd<int8_t>(const int8_t value) throw() {
+		enum : int8_t { MASK = ~1 };
+		return value & MASK;
+	}
+
+	template<>
+	static inline float RoundDownOdd<float>(const float value) throw() {
+		return static_cast<float>(RoundDownOdd(static_cast<int64_t>(value)));
+	}
+
+	template<>
+	static inline double RoundDownOdd<double>(const double value) throw() {
+		return static_cast<double>(RoundDownOdd(static_cast<int64_t>(value)));
+	}
+
+	// RoundUpEven
+
+	template<>
+	static inline uint64_t RoundUpEven<uint64_t>(const uint64_t value) throw() {
+		return value + (value & 1ull);
+	}
+
+	template<>
+	static inline uint32_t RoundUpEven<uint32_t>(const uint32_t value) throw() {
+		return value + (value & 1u);
+	}
+
+	template<>
+	static inline uint16_t RoundUpEven<uint16_t>(const uint16_t value) throw() {
+		return value + (value & 1u);
+	}
+
+	template<>
+	static inline uint8_t RoundUpEven<uint8_t>(const uint8_t value) throw() {
+		return value + (value & 1u);
+	}
+
+	template<>
+	static inline int64_t RoundUpEven<int64_t>(const int64_t value) throw() {
+		return value + (value & 1ll);
+	}
+
+	template<>
+	static inline int32_t RoundUpEven<int32_t>(const int32_t value) throw() {
+		return value + (value & 1);
+	}
+
+	template<>
+	static inline int16_t RoundUpEven<int16_t>(const int16_t value) throw() {
+		return value + (value & 1);
+	}
+
+	template<>
+	static inline int8_t RoundUpEven<int8_t>(const int8_t value) throw() {
+		return value + (value & 1);
+	}
+
+	template<>
+	static inline float RoundUpEven<float>(const float value) throw() {
+		return static_cast<float>(RoundUpEven(static_cast<int64_t>(value)));
+	}
+
+	template<>
+	static inline double RoundUpEven<double>(const double value) throw() {
+		return static_cast<double>(RoundUpEven(static_cast<int64_t>(value)));
+	}
+
+	// RoundDownEven
+
+	template<>
+	static inline uint64_t RoundDownEven<uint64_t>(const uint64_t value) throw() {
+		return value - (value & 1ull);
+	}
+
+	template<>
+	static inline uint32_t RoundDownEven<uint32_t>(const uint32_t value) throw() {
+		return value - (value & 1u);
+	}
+
+	template<>
+	static inline uint16_t RoundDownEven<uint16_t>(const uint16_t value) throw() {
+		return value - (value & 1u);
+	}
+
+	template<>
+	static inline uint8_t RoundDownEven<uint8_t>(const uint8_t value) throw() {
+		return value - (value & 1u);
+	}
+
+	template<>
+	static inline int64_t RoundDownEven<int64_t>(const int64_t value) throw() {
+		return value - (value & 1ll);
+	}
+
+	template<>
+	static inline int32_t RoundDownEven<int32_t>(const int32_t value) throw() {
+		return value - (value & 1);
+	}
+
+	template<>
+	static inline int16_t RoundDownEven<int16_t>(const int16_t value) throw() {
+		return value - (value & 1);
+	}
+
+	template<>
+	static inline int8_t RoundDownEven<int8_t>(const int8_t value) throw() {
+		return value - (value & 1);
+	}
+
+	template<>
+	static inline float RoundDownEven<float>(const float value) throw() {
+		return static_cast<float>(RoundDownEven(static_cast<int64_t>(value)));
+	}
+
+	template<>
+	static inline double RoundDownEven<double>(const double value) throw() {
+		return static_cast<double>(RoundDownEven(static_cast<int64_t>(value)));
 	}
 }
 
