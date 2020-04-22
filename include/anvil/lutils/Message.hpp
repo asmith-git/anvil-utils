287f4272d7783d35db6c358173bab4a8752c5a7d
Adam Smith <adam.george.smith@btinternet.com>
2020-04-22 22:29:40 +0100

    Message ID Reuse
    Added compile-time option to re-use message IDs
    (ANVIL_LUTILS_REUSE_MESSAGE_IDS)

--- include/anvil/lutils/Message.hpp
+++ include/anvil/lutils/Message.hpp
@@ -56,7 +56,11 @@ namespace anvil { namespace lutils { namespace msg {
 	class Queue {
 	private:
 		std::atomic_uint32_t _recursion_counter;
+#ifdef ANVIL_LUTILS_REUSE_MESSAGE_IDS
+		IDGeneratorSelector<uint64_t, true, false, true> _id_generator;
+#else
 		IDGeneratorSelector<std::atomic_uint64_t, false, false, false> _id_generator;
+#endif
 		std::recursive_mutex _consumer_mutex;
 		std::recursive_mutex _message_mutex;
 		PODVectorDynamic<Consumer*> _consumers;
