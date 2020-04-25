513714737c349aa43b1eda726d9078310e7a5781
Adam Smith <adam.george.smith@btinternet.com>
2020-04-25 12:34:53 +0100

    Blend SSE
    Float32 blend implemented for SSE and SSE 4.1

--- include/anvil/lutils/asm/ASM_Blend.hpp
+++ include/anvil/lutils/asm/ASM_Blend.hpp
@@ -174,6 +174,71 @@ namespace anvil { namespace lutils { namespace experimental {
 
 	// x86 Implementation
 
+#if ANVIL_EXPERIMENTAL_X86
+	namespace detail {
+
+		template<uint64_t MASK, size_t S>
+		struct MaskHelper32;
+
+		template<uint64_t MASK>
+		struct MaskHelper32<MASK, 4u> {
+			enum : int {
+				M0 = (MASK & 1ull) == 0ull ? 0 : -1,
+				M1 = (MASK & 2ull) == 0ull ? 0 : -1,
+				M2 = (MASK & 4ull) == 0ull ? 0 : -1,
+				M3 = (MASK & 8ull) == 0ull ? 0 : -1
+			};
+
+			static constexpr const int32_t g_mask[4u] = { M0, M1, M2, M3 };
+		};
+
+		static constexpr bool UsingSSE41(InstructionSets is) {
+			enum : InstructionSets {
+				MASK = ~(ANVIL_SSE | ANVIL_SSE2 | ANVIL_SSE3 | ANVIL_SSSE3)
+			};
+			return (is & MASK) != 0ull;
+		}
+
+		
+
+		template<uint64_t MASK, InstructionSets IS, bool = UsingSSE41(IS)>
+		struct BlendF32SSE;
+
+		template<uint64_t MASK, InstructionSets IS>
+		struct BlendF32SSE<MASK, IS, false> {
+		private:
+			enum : uint64_t { MASK2 = MASK & DefaultMask<__m128>::value };
+			const __m128 _mask;
+		public:
+			BlendF32SSE() :
+				_mask(_mm_loadu_ps(reinterpret_cast<const float*>(MaskHelper32<MASK2, 4u>::g_mask)))
+			{}
+
+			inline __m128 operator()(const __m128 src, const __m128 other) const throw() {
+				return _mm_or_ps(_mm_andnot_ps(_mask, src), _mm_and_ps(_mask, other));
+			}
+		};
+
+		template<uint64_t MASK, InstructionSets IS>
+		struct BlendF32SSE<MASK, IS, true> {
+			inline __m128 operator()(const __m128 src, const __m128 other) const throw() {
+				enum : int { MASK2 = static_cast<int>(MASK & DefaultMask<__m128>::value) };
+				return _mm_blend_ps(src, other, MASK2);
+			}
+		};
+	}
+#endif
+	
+
+	template<const uint64_t MASK, InstructionSets IS>
+	struct Blend<MASK, __m128, IS> {
+	private:
+		const detail::BlendF32SSE<MASK, IS> _blend;
+	public:
+		inline __m128 operator()(const __m128 src, const __m128 other) const throw() {
+			return _blend(src, other);
+		}
+	};
 
 }}}
 
