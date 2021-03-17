3bde2f1e4d3770e7b3a590bf608678ccc7ca0ec7
Adam Smith <adam.george.smith@btinternet.com>
2021-03-17 20:23:56 +0000

    Serialise output to JSON

---
+++ include/anvil/byte-pipe/BytePipeJSON.hpp
@@ -0,0 +1,68 @@
+//Copyright 2021 Adam G. Smith
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http ://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#ifndef ANVIL_LUTILS_BYTEPIPE_JSON_HPP
+#define ANVIL_LUTILS_BYTEPIPE_JSON_HPP
+
+#include <vector>
+#include <string>
+#include "anvil/byte-pipe/BytePipeReader.hpp"
+
+namespace anvil { namespace BytePipe {
+
+	/*!
+		\author Adam Smith
+		\date March 2021
+		\brief Converts a BytePipe serialisation into a JSON string
+	*/
+	class JsonWriter final : public Parser {
+	private:
+		mutable std::string _out;
+	private:
+		void AddValue(Type type, std::string&& val);
+	public:
+		JsonWriter();
+		virtual ~JsonWriter();
+
+		const std::string& GetJSON() const;
+
+		// Inherited from Parser
+
+		void OnPipeOpen() final;
+		void OnPipeClose() final;
+		void OnArrayBegin(const uint32_t size) final;
+		void OnArrayEnd() final;
+		void OnObjectBegin(const uint32_t component_count) final;
+		void OnObjectEnd() final;
+		void OnComponentID(const ComponentID id)  final;
+		void OnUserPOD(const uint32_t type, const uint32_t bytes, const void* data) final;
+		void OnNull() final;
+		void OnPrimativeF64(const double value) final;
+		void OnPrimativeString(const char* value, const uint32_t length) final;
+		void OnPrimativeBool(const bool value) final;
+		void OnPrimativeC8(const char value) final;
+		void OnPrimativeU64(const uint64_t value) final;
+		void OnPrimativeS64(const int64_t value) final;
+		void OnPrimativeF32(const float value) final;
+		void OnPrimativeU8(const uint8_t value) final;
+		void OnPrimativeU16(const uint16_t value) final;
+		void OnPrimativeU32(const uint32_t value) final;
+		void OnPrimativeS8(const int8_t value) final;
+		void OnPrimativeS16(const int16_t value) final;
+		void OnPrimativeS32(const int32_t value) final;
+	};
+
+}}
+
+#endif
---
+++ src/anvil/byte-pipe/BinaryPipeJSON.cpp
@@ -0,0 +1,143 @@
+//Copyright 2021 Adam G. Smith
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http ://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+#include "anvil/byte-pipe/BytePipeJSON.hpp"
+
+namespace anvil { namespace BytePipe {
+
+	JsonWriter::JsonWriter() {
+
+	}
+
+	JsonWriter::~JsonWriter() {
+
+	}
+
+	const std::string& JsonWriter::GetJSON() const {
+		return _out;
+	}
+
+	// Inherited from Parser
+
+	void JsonWriter::OnPipeOpen() {
+		// Reset object state
+		_out.clear();
+	}
+
+	void JsonWriter::OnPipeClose() {
+		// Do nothing
+	}
+
+	void JsonWriter::OnArrayBegin(const uint32_t size) {
+		_out += '[';
+	}
+
+	void JsonWriter::OnArrayEnd() {
+		if (_out.back() == ',') _out.pop_back();
+		_out += ']';
+	}
+
+	void JsonWriter::OnObjectBegin(const uint32_t component_count) {
+		_out += '{';
+	}
+
+	void JsonWriter::OnObjectEnd() {
+		if (_out.back() == ',') _out.pop_back();
+		_out += '}';
+	}
+
+	void JsonWriter::OnComponentID(const ComponentID id) {
+		_out += '"' + std::to_string(id) + "\":";
+	}
+
+
+	void JsonWriter::AddValue(Type type, std::string&& val) {
+		if (_out.empty()) {
+			_out.swap(val);
+		} else {
+			_out += val;
+		}
+		_out += ",";
+	}
+
+	void JsonWriter::OnUserPOD(const uint32_t type, const uint32_t bytes, const void* data) {
+		// Format the POD as an object, a POD is identified by containg the member __ANVIL_POD with the value 123456789
+		std::string value = "{\"__ANVIL_POD\":123456789,\"type\":" + std::to_string(type) + ",\bytes\":" + std::to_string(bytes) + ",\data\":[";
+		for (uint32_t i = 0u; i < bytes; ++i) {
+			value += std::to_string(reinterpret_cast<const uint8_t*>(data)[i]) + ",";
+		}
+		if (value.back() == ',') value.pop_back();
+		value += "]}";
+		
+		// Add the value
+		AddValue(TYPE_OBJECT, std::move(value));
+	}
+
+	void JsonWriter::OnNull() {
+		AddValue(TYPE_NULL, "null");
+	}
+
+	void JsonWriter::OnPrimativeF64(const double value) {
+		AddValue(TYPE_F64, std::to_string(value));
+	}
+
+	void JsonWriter::OnPrimativeString(const char* value, const uint32_t length) {
+		AddValue(TYPE_STRING, std::string(value, value + length));
+	}
+
+	void JsonWriter::OnPrimativeBool(const bool value) {
+		AddValue(TYPE_BOOL, value ? "True" : "False");
+	}
+
+	void JsonWriter::OnPrimativeC8(const char value) {
+		OnPrimativeString(&value, 1u);
+	}
+
+	void JsonWriter::OnPrimativeU64(const uint64_t value) {
+		AddValue(TYPE_U64, std::to_string(value));
+	}
+
+	void JsonWriter::OnPrimativeS64(const int64_t value) {
+		AddValue(TYPE_S64, std::to_string(value));
+	}
+
+	void JsonWriter::OnPrimativeF32(const float value) {
+		AddValue(TYPE_F64, std::to_string(value));
+	}
+
+	void JsonWriter::OnPrimativeU8(const uint8_t value) {
+		AddValue(TYPE_U8, std::to_string(value));
+	}
+
+	void JsonWriter::OnPrimativeU16(const uint16_t value) {
+		AddValue(TYPE_U16, std::to_string(value));
+	}
+
+	void JsonWriter::OnPrimativeU32(const uint32_t value) {
+		AddValue(TYPE_U32, std::to_string(value));
+	}
+
+	void JsonWriter::OnPrimativeS8(const int8_t value) {
+		AddValue(TYPE_S8, std::to_string(value));
+	}
+
+	void JsonWriter::OnPrimativeS16(const int16_t value) {
+		AddValue(TYPE_S16, std::to_string(value));
+	}
+
+	void JsonWriter::OnPrimativeS32(const int32_t value) {
+		AddValue(TYPE_S32, std::to_string(value));
+	}
+
+}}
